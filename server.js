72d07b0237464cf4e2f98fea353379ef92b69d58
Wai Yan Linn <134668458+DevWaiYanLinn@users.noreply.github.com>
2023-10-06 14:48:20 +0630

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('user is conneted')
     socket.on('message', (msg) => {
         io.emit('message', msg);
     });
