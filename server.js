9f9b29fc6b14665529f0feccd561ff4142dc7f70
Wai Yan Lin <waiyanlinn.lion@outlook.com>
2023-10-06 14:45:49 +0630

    console.log

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-
+    console.log('user is conneted')
     socket.on('message', (msg) => {
         io.emit('message', msg);
     });
